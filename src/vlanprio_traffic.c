70d0fc023e5a7181b889c88f2bae88a9deb3d384
Andre Kantek <andrekantek@gmail.com>
2012-11-17 10:19:00 -0200

    adding better command line parser

--- src/vlanprio_traffic.c
+++ src/vlanprio_traffic.c
@@ -20,7 +20,25 @@
 #include <libnet/libnet-structures.h>
 #include <libnet/libnet-types.h>
 
+#include <glib.h>
+
 #define BASE_DECIMAL 10
+
+#define TPID_VLAN_8100          0x8100
+#define TPID_VLAN_88A8          0x88a8
+#define TPID_VLAN_9100          0x9100
+#define TPID_VLAN_9200          0x9200
+#define TPID_VLAN_9300          0x9300
+
+#define TPID_VLAN_8100_STR          "0x8100"
+#define TPID_VLAN_88A8_STR          "0x88a8"
+#define TPID_VLAN_9100_STR          "0x9100"
+#define TPID_VLAN_9200_STR          "0x9200"
+#define TPID_VLAN_9300_STR          "0x9300"
+
+
+#define BUFFER_CHAR 256
+
 #define libnet_timersub(tvp, uvp, vvp)                                  \
         do {                                                            \
                 (vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;          \
@@ -31,25 +49,116 @@
                 }                                                       \
         } while (0)
 
-/*
- ============================================================================
- */
-static void usage(char *name) {
-	fprintf(stdout,
-			"usage %s -v [vlan-id] -q [vlan-prio] -x [rotate vlan-prio] \n"
-			"\t -i [interface] -a [srcMAC] -s [srcIP.port] -b [dstMac] -d [dstIP.oprt] \n"
-			"\t -p [payload size] -t [tx rate (bps)] -r [# of packets]\n", name);
-}
 
 /*
  ============================================================================
  */
 int main(int argc, char *argv[]) {
 
-	printf("started\n");
+	printf("=====  started ======\n");
+
+	gchar *interface = "eth0";
+	gint svid = 0;
+	gint svidprio = -1;
+	gboolean svidrotate = FALSE;
+	gchar *stpid = "0x8100";
+	gint stpid_val = TPID_VLAN_8100;
+	gint cvid     = 0;
+	gint cvidprio = -1;
+	gboolean cvidrotate = FALSE;
+	gchar *macsrc = "00:00:00:00:00:00";
+	gchar *macdst = "00:00:00:00:00:00";
+	gchar *ipdst  = "0.0.0.0.0";
+	gchar *ipsrc  = "0.0.0.0.0";
+	gint udpsrc = 0;
+	gint udpdst = 0;
+	gint pktsize = 0;
+	gint txrate = 0;
+	gint packets = 0;
+
+	GOptionEntry cmd_entries[] = {
+		{ "interface"  , 'i', 0, G_OPTION_ARG_STRING , &interface  , "interface", NULL },
+		{ "svid"       ,  0 , 0, G_OPTION_ARG_INT    , &svid       , "svlan id [1-4096]", NULL },
+		{ "svidprio"   ,  0 , 0, G_OPTION_ARG_INT    , &svidprio   , "svlan priority [0-7]", NULL },
+		{ "svidrotate" ,  0 , 0, G_OPTION_ARG_NONE   , &svidrotate , "rotate svlan priority", NULL },
+		{ "stpid"      ,  0 , 0, G_OPTION_ARG_STRING , &stpid      , "svlan tpid val [0x8100, 0x88a8, 0x9100, 0x9200, 0x9300]", NULL },
+		{ "cvid"       ,  0 , 0, G_OPTION_ARG_INT    , &cvid       , "cvlan id [1-4096]", NULL },
+		{ "cvidprio"   ,  0 , 0, G_OPTION_ARG_INT    , &cvidprio   , "cvlan priority [0-7]", NULL },
+		{ "cvidrotate" ,  0 , 0, G_OPTION_ARG_NONE   , &cvidrotate , "rotate cvlan priority", NULL },
+		{ "macsrc"     ,  0 , 0, G_OPTION_ARG_STRING , &macsrc     , "source mac address", NULL },
+		{ "macdst"     ,  0 , 0, G_OPTION_ARG_STRING , &macdst     , "destination mac address", NULL },
+		{ "ipsrc"      ,  0 , 0, G_OPTION_ARG_STRING , &ipsrc      , "source IP ", NULL },
+		{ "udpsrc"     ,  0 , 0, G_OPTION_ARG_INT    , &udpsrc     , "source UDP port", NULL },
+		{ "ipdst"      ,  0 , 0, G_OPTION_ARG_STRING , &ipdst      , "dest IP ", NULL },
+		{ "udpdst"     ,  0 , 0, G_OPTION_ARG_INT    , &udpdst     , "dest UDP port", NULL },
+		{ "pktsize"    , 's', 0, G_OPTION_ARG_INT    , &pktsize    , "UDP payload size in bytes", NULL },
+		{ "txrate"     , 't', 0, G_OPTION_ARG_INT    , &txrate     , "transmission rate in bps", NULL },
+		{ "packets"    , 'p', 0, G_OPTION_ARG_INT    , &packets    , "number of packets for tx", NULL },
+		{ NULL }
+	};
+
+	GError *error = NULL;
+	GOptionContext *context;
+
+	context = g_option_context_new("vlan priority traffic generator");
+	g_option_context_add_main_entries(context, cmd_entries, NULL);
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
+		g_print("option parsing failed: %s\n", error->message);
+		exit(EXIT_FAILURE);
+	}
+	// checks
+	if (svid < 1 || svid > 4096) {
+		fprintf(stderr, "invalid svid: %d\n", svid);
+		return (EXIT_FAILURE);
+	}
+	if (svidprio < 0 || svidprio > 7) {
+		fprintf(stderr, "invalid svidprio: %d\n", svidprio);
+		return (EXIT_FAILURE);
+	}
+	if (cvid < 1 || cvid > 4096) {
+		fprintf(stderr, "invalid cvid: %d\n", cvid);
+		return (EXIT_FAILURE);
+	}
+	if (cvidprio < 0 || cvidprio > 7) {
+		fprintf(stderr, "invalid cvidprio: %d\n", cvidprio);
+		return (EXIT_FAILURE);
+	}
+
+	g_ascii_strdown(stpid,strlen(stpid));
+	if (g_strcmp0(stpid, TPID_VLAN_8100_STR) == 0) {
+		stpid_val = TPID_VLAN_8100;
+	} else if (g_strcmp0(stpid, TPID_VLAN_88A8_STR) == 0) {
+		stpid_val = TPID_VLAN_88A8;
+	} else if (g_strcmp0(stpid, TPID_VLAN_9100_STR) == 0) {
+		stpid_val = TPID_VLAN_9100;
+	} else if (g_strcmp0(stpid, TPID_VLAN_9200_STR) == 0) {
+		stpid_val = TPID_VLAN_9200;
+	} else if (g_strcmp0(stpid, TPID_VLAN_9300_STR) == 0) {
+		stpid_val = TPID_VLAN_9300;
+	} else {
+		fprintf(stderr, "invalid stpid: %s\n", stpid);
+		return (EXIT_FAILURE);
+	}
+
+	g_print("interface   %s\n",interface) ;
+	g_print("svid        %d\n",svid) ;
+	g_print("svidprio    %d\n",svidprio) ;
+	g_print("svidrotate  %d\n",svidrotate) ;
+	g_print("stpid       %s\n",stpid) ;
+	g_print("cvid        %d\n",cvid) ;
+	g_print("cvidprio    %d\n",cvidprio) ;
+	g_print("cvidrotate  %d\n",cvidrotate) ;
+	g_print("macdst      %s\n",macdst) ;
+	g_print("ipdst       %s\n",ipdst) ;
+	g_print("udpdst      %d\n",udpdst) ;
+	g_print("macsrc      %s\n",macsrc) ;
+	g_print("ipsrc       %s\n",ipsrc) ;
+	g_print("udpsrc      %d\n",udpsrc) ;
+	g_print("pktsize     %d\n",pktsize) ;
+	g_print("txrate      %d\n",txrate) ;
+    g_print("packets     %d\n",packets) ;
 
 	libnet_t *lnet;
-	char *pEnd = NULL;
 	char *host_dst = "2.2.2.2";
 	char *host_src = "1.1.1.1";
 	int len;
@@ -61,17 +170,17 @@ int main(int argc, char *argv[]) {
 	struct timeval end_time;
 	int repeat = 0;
 	int repeat_max = 0;
-	int rotate_vlan_prio = 0;
+	int rotate_svlan_prio = 0;
 	u_int32_t loop_delay = 80;
 
 	// layer 2
-	libnet_ptag_t vlan_ptag = 0;
-	u_int8_t vlan_cfi_flag = 0;
 	u_char *mac_dst, *mac_src;
-	u_int8_t *vlan_payload = NULL;
-	u_int32_t vlan_payload_s = 0;
-	u_int8_t vlan_prio;
-	u_int16_t vlan_id;
+	libnet_ptag_t svlan_ptag = 0;
+	u_int8_t svlan_cfi_flag = 0;
+	u_int8_t *svlan_payload = NULL;
+	u_int32_t svlan_payload_s = 0;
+	u_int8_t svlan_prio;
+	u_int16_t svlan_id;
 
 	// layer 3
 	libnet_ptag_t ip_ptag = 0;
@@ -94,97 +203,22 @@ int main(int argc, char *argv[]) {
 	u_int8_t *udp_payload; // ==> user payload
 	u_int32_t udp_payload_s;
 
-	//====================================================================
-	/*
-	 *  Initialize the library.  Root priviledges are required.
-	 */
 
-	printf("parse input\n");
-	char *cp;
-	int c;
-	while ((c = getopt(argc, argv, "v:q:a:b:d:s:p:i:t:r:x:")) != EOF) {
-		switch (c) {
-		case 'v':
-			vlan_id = strtol(optarg, &pEnd, BASE_DECIMAL);
-			if (vlan_id < 1 || vlan_id > 4096) {
-				fprintf(stderr, "invalid vlan-id: %s\n", optarg);
-				return (EXIT_FAILURE);
-			}
-			printf("v: vlan-id=%d\n", vlan_id);
-			break;
+    mac_src = libnet_hex_aton(macsrc, &len);
+    mac_dst = libnet_hex_aton(macdst, &len);
+    svlan_id = (u_int16_t) svid;
+    svlan_prio = (u_int8_t) svidprio;
+    udp_src_prt = (u_int16_t) udpsrc;
+    udp_dst_prt = (u_int16_t) udpdst;
+    eth_device = interface;
+    udp_payload_s = pktsize;
+    tx_speed_bps = txrate;
+    repeat_max = packets;
+    rotate_svlan_prio = (int) svidrotate;
+    host_dst = ipdst;
+    host_src = ipsrc;
 
-		case 'q': // vlan-prio (tx-Queue)
-			vlan_prio = strtol(optarg, &pEnd, BASE_DECIMAL);
-			if (vlan_prio < 0 || vlan_prio > 7) {
-				fprintf(stderr, "invalid vlan-prio: %s\n", optarg);
-				return (EXIT_FAILURE);
-			}
-			printf("q: vlan-prio=%d\n", vlan_prio);
-			break;
-
-		case 'a': // source-mac
-			mac_src = libnet_hex_aton(optarg, &len);
-			printf("b: mac_src=[%02X:%02X:%02X:%02X:%02X:%02X]\n", mac_src[0],
-					mac_src[1], mac_src[2], mac_src[3], mac_src[4], mac_src[5]);
-			break;
-
-		case 'b': // dest-mac
-			mac_dst = libnet_hex_aton(optarg, &len);
-			printf("b: mac_dst=[%02X:%02X:%02X:%02X:%02X:%02X]\n", mac_dst[0],
-					mac_dst[1], mac_dst[2], mac_dst[3], mac_dst[4], mac_dst[5]);
-			break;
-
-		case 's': // source-ip
-			if (!(cp = strrchr(optarg, '.'))) {
-				usage(argv[0]);
-			}
-			*cp++ = 0;
-			udp_src_prt = (u_int16_t) strtol(cp, &pEnd, BASE_DECIMAL);
-			host_src = optarg;
-			printf("s: udp_src_prt=%d, ip_src=%s\n", udp_src_prt, optarg);
-			break;
-
-		case 'd': // dest-ip
-			if (!(cp = strrchr(optarg, '.'))) {
-				usage(argv[0]);
-			}
-			*cp++ = 0;
-			udp_dst_prt = (u_int16_t) strtol(cp, &pEnd, BASE_DECIMAL);
-			host_dst = optarg;
-			printf("d: udp_dst_prt=%d, ip_dst=%s\n", udp_dst_prt, optarg);
-			break;
-
-		case 'i': // interface
-			eth_device = optarg;
-			printf("i: eth_device=%s\n", eth_device);
-			break;
-
-		case 'p':
-			udp_payload_s = strtol(optarg, &pEnd, BASE_DECIMAL);
-			printf("p: udp_payload_s=%d\n", udp_payload_s);
-			break;
-
-		case 't':
-			tx_speed_bps = strtol(optarg, &pEnd, BASE_DECIMAL);
-			printf("t: tx_speed_bps=%d\n", tx_speed_bps);
-			break;
-
-		case 'r':
-			repeat_max = strtol(optarg, &pEnd, BASE_DECIMAL);
-			printf("r: repeat_max=%d\n", repeat_max);
-			break;
-
-		case 'x':
-			rotate_vlan_prio = 1;
-			printf("x: rotate_vlan_prio=%d\n", rotate_vlan_prio);
-			break;
-
-		default:
-			usage(argv[0]);
-			exit(EXIT_FAILURE);
-		}
-	}
-
+	//=====================================================================
 	printf("init libnet\n");
 	lnet = libnet_init(LIBNET_LINK, eth_device, errbuf);
 	if (lnet == NULL) {
@@ -235,10 +269,10 @@ int main(int argc, char *argv[]) {
 
 	//====================================================================
 	printf("libnet_build_802_1q\n");
-	vlan_ptag = libnet_build_802_1q(mac_dst, mac_src, ETHERTYPE_VLAN, vlan_prio,
-			vlan_cfi_flag, vlan_id, ETHERTYPE_IP, vlan_payload, vlan_payload_s,
+	svlan_ptag = libnet_build_802_1q(mac_dst, mac_src, stpid_val, svlan_prio,
+			svlan_cfi_flag, svlan_id, ETHERTYPE_IP, svlan_payload, svlan_payload_s,
 			lnet, 0);
-	if (vlan_ptag == -1) {
+	if (svlan_ptag == -1) {
 		fprintf(stderr, "Can't build 802.1q header: %s\n",
 				libnet_geterror(lnet));
 		goto bad;
@@ -265,14 +299,14 @@ int main(int argc, char *argv[]) {
 			goto bad;
 		}
 
-		if (rotate_vlan_prio==1) {
+		if (rotate_svlan_prio==1) {
 			// modify packet
-			if (vlan_prio == 7) {
-				vlan_prio = 0;
-				udp_prio_src_prt = udp_src_prt + vlan_prio;
-				udp_prio_dst_prt = udp_dst_prt + vlan_prio;
+			if (svlan_prio == 7) {
+				svlan_prio = 0;
+				udp_prio_src_prt = udp_src_prt + svlan_prio;
+				udp_prio_dst_prt = udp_dst_prt + svlan_prio;
 			} else {
-				vlan_prio++;
+				svlan_prio++;
 				udp_prio_src_prt++;
 				udp_prio_dst_prt++;
 			}
@@ -282,9 +316,9 @@ int main(int argc, char *argv[]) {
 				udp_pkt_len, udp_checksum, udp_payload, udp_payload_s, lnet,
 				udp_ptag);
 
-		vlan_ptag = libnet_build_802_1q(mac_dst, mac_src, ETHERTYPE_VLAN,
-				vlan_prio, vlan_cfi_flag, vlan_id, ETHERTYPE_IP, vlan_payload,
-				vlan_payload_s, lnet, vlan_ptag);
+		svlan_ptag = libnet_build_802_1q(mac_dst, mac_src, ETHERTYPE_VLAN,
+				svlan_prio, svlan_cfi_flag, svlan_id, ETHERTYPE_IP, svlan_payload,
+				svlan_payload_s, lnet, svlan_ptag);
 
 
 
